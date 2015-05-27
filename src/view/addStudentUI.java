a0d6f5b16b069f8396994525a646466ebf3a6232
robertoramsis <robertoramsis@yahoo.com>
2015-05-26 22:26:25 -0700

    Adding addStudent UI/Controller

---
+++ src/view/addStudentUI.java
@@ -0,0 +1,20 @@
+package view;
+import model.*;
+import javax.swing.*;
+
+public class addStudentUI {
+	private JFrame window;
+	private JPanel topPanel;
+	private JButton add;
+	private JButton delete;
+	private JButton edit;
+	private Student addedStudent;
+	
+	public addStudentUI(){
+		
+	}
+	
+	public void registerEvents(){
+		
+	}
+}
