b88f76f230b6673c8b38414f9054740454fe3ec7
robertoramsis <robertoramsis@yahoo.com>
2015-05-27 16:15:46 -0700

    Adding JPA fields

--- src/view/SetupUI.java
+++ src/view/SetupUI.java
@@ -1,14 +1,20 @@
-package src.view;
+package view;
 
 import javax.swing.JFrame;
+import controller.*;
 import javax.swing.JPanel;
 
 public class SetupUI {
 	private JFrame window;
 	private JPanel topPanel;
-	
+	private String PERSISTENCE_UNIT_NAME="PersistenceUnit";
+	EntityManagerFactory factory = Persistence.createEntityManagerFactory(PERSISTENCE_UNIT_NAME);
+	EntityManager manager = factory.createEntityManager();
+	GameService gameService = new GameService(manager);
+	SongService songService = new SongService(manager);
+	StoryService storyService = new StoryService(manager);
 	public SetupUI() {
-		
+		new AdminSetupUI();
 	}
 	
 	public static void main (String[] args) {
