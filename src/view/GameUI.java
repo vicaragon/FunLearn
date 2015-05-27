02aaa84b774f840f4c7d29df478e861c0685bdd2
robertoramsis <robertoramsis@yahoo.com>
2015-05-27 14:25:02 -0700

    Adding instance of game entity/controller

--- src/view/GameUI.java
+++ src/view/GameUI.java
@@ -1,14 +1,17 @@
 package view;
 import javax.swing.*;
+import controller.*;
+import model.*;
 
 public class GameUI {
+        private Game game;
+	private GameController gameController;
 	private JFrame window;
 	private JPanel panel;
 	private JButton selectGame;
 	private JButton playGame;
 	private JButton endGame;
 	private JLabel scores;
-	
 	public GameUI(){};
 	public void registerEvents(){
 		
