8ce7f87cce1a4b5aec0c828fbb5d4d911cd7b0ef
Sonam Rudraraju <sonam.rks@gmail.com>
2015-05-28 01:25:56 -0700

    GUI changes

--- src/view/SetupUI.java
+++ src/view/SetupUI.java
@@ -6,7 +6,11 @@ import javax.swing.JPanel;
 
 public class SetupUI {
 	private JFrame window;
-	private JPanel topPanel;
+	private JPanel cards;
+	private JPanel studentListUI;
+	private JPanel gameTypeUI;
+	private JPanel levelTypeUI;
+	private JPanel startUI;
 	private String PERSISTENCE_UNIT_NAME="PersistenceUnit";
 	EntityManagerFactory factory = Persistence.createEntityManagerFactory(PERSISTENCE_UNIT_NAME);
 	EntityManager manager = factory.createEntityManager();
@@ -14,7 +18,10 @@ public class SetupUI {
 	SongService songService = new SongService(manager);
 	StoryService storyService = new StoryService(manager);
 	public SetupUI() {
-		new AdminSetupUI();
+		cards.add(studentListUI);
+		cards.add(gameTypeUI);
+		cards.add(levelTypeUI);
+		cards.add(startUI);
 	}
 	
 	public static void main (String[] args) {
