d6e1cf229e65c1f632a7a3d4715627c1cc5c287a
robertoramsis <robertoramsis@yahoo.com>
2015-05-30 12:36:06 -0700

    Adding JPA Entity  Song/Story

--- src/model/Song.java
+++ src/model/Song.java
@@ -1,73 +1,120 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
 package model;
 
 import java.io.Serializable;
-import java.util.List;
+import javax.persistence.Basic;
+import javax.persistence.Column;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.MappedSuperclass;
+import javax.persistence.Table;
+import javax.persistence.UniqueConstraint;
+import javax.xml.bind.annotation.XmlRootElement;
 
+/**
+ *
+ * @author me
+ */
+@MappedSuperclass
+@Table(catalog = "FunLearnDB", schema = "", uniqueConstraints = {
+    @UniqueConstraint(columnNames = {"songID"})})
+@XmlRootElement
 public class Song implements Serializable {
-	/**
-	 * 
-	 */
-	private static final long serialVersionUID = 4875581258729443007L;
-	private int songID;
-	private String songName;
-	private int ageGroup;
-	private String language;
-	private String songAudioPath;
-	private List<String> songImagePath;
-	/**
-	 * @param songID
-	 * @param songName
-	 * @param ageGroup
-	 * @param language
-	 * @param songAudioPath
-	 * @param songImagePath
-	 */
-	public Song(int songID, String songName, int ageGroup, String language,
-			String songAudioPath, List<String> songImagePath) {
-		this.songID = songID;
-		this.songName = songName;
-		this.ageGroup = ageGroup;
-		this.language = language;
-		this.songAudioPath = songAudioPath;
-		this.songImagePath = songImagePath;
-	}
-	public Song() {
-		
-	}
-	public int getSongID() {
-		return songID;
-	}
-	public void setSongID(int songID) {
-		this.songID = songID;
-	}
-	public String getSongName() {
-		return songName;
-	}
-	public void setSongName(String songName) {
-		this.songName = songName;
-	}
-	public int getAgeGroup() {
-		return ageGroup;
-	}
-	public void setAgeGroup(int ageGroup) {
-		this.ageGroup = ageGroup;
-	}
-	public String getLanguage() {
-		return language;
-	}
-	public void setLanguage(String language) {
-		this.language = language;
-	}
-	public String getSongAudioPath() {
-		return songAudioPath;
-	}
-	public void setSongAudioPath(String songAudioPath) {
-		this.songAudioPath = songAudioPath;
-	}
-	public List<String> getSongImagePath() {
-		return songImagePath;
-	}
-	public void setSongImagePath(List<String> songImagePath) {
-		this.songImagePath = songImagePath;
-	}
+    private static final long serialVersionUID = 1L;
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Basic(optional = false)
+    @Column(nullable = false)
+    private Integer songID;
+    @Basic(optional = false)
+    @Column(nullable = false, length = 45)
+    private String songName;
+    private Integer ageGroup;
+    @Column(length = 45)
+    private String language;
+    @Column(length = 45)
+    private String songVideoPath;
+
+    public Song() {
+    }
+
+    public Song(Integer songID) {
+        this.songID = songID;
+    }
+
+    public Song(Integer songID, String songName) {
+        this.songID = songID;
+        this.songName = songName;
+    }
+
+    public Integer getSongID() {
+        return songID;
+    }
+
+    public void setSongID(Integer songID) {
+        this.songID = songID;
+    }
+
+    public String getSongName() {
+        return songName;
+    }
+
+    public void setSongName(String songName) {
+        this.songName = songName;
+    }
+
+    public Integer getAgeGroup() {
+        return ageGroup;
+    }
+
+    public void setAgeGroup(Integer ageGroup) {
+        this.ageGroup = ageGroup;
+    }
+
+    public String getLanguage() {
+        return language;
+    }
+
+    public void setLanguage(String language) {
+        this.language = language;
+    }
+
+    public String getSongVideoPath() {
+        return songVideoPath;
+    }
+
+    public void setSongVideoPath(String songVideoPath) {
+        this.songVideoPath = songVideoPath;
+    }
+
+    @Override
+    public int hashCode() {
+        int hash = 0;
+        hash += (songID != null ? songID.hashCode() : 0);
+        return hash;
+    }
+
+    @Override
+    public boolean equals(Object object) {
+        // TODO: Warning - this method won't work in the case the id fields are not set
+        if (!(object instanceof Song)) {
+            return false;
+        }
+        Song other = (Song) object;
+        if ((this.songID == null && other.songID != null) || (this.songID != null && !this.songID.equals(other.songID))) {
+            return false;
+        }
+        return true;
+    }
+
+    @Override
+    public String toString() {
+        return "model.Song[ songID=" + songID + " ]";
+    }
+    
 }
--- src/model/Story.java
+++ src/model/Story.java
@@ -1,93 +1,123 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
 package model;
+
 import java.io.Serializable;
-import java.util.Map;
+import javax.persistence.Basic;
+import javax.persistence.Column;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.MappedSuperclass;
+import javax.persistence.Table;
+import javax.xml.bind.annotation.XmlRootElement;
 
-
+/**
+ *
+ * @author me
+ */
+@MappedSuperclass
+@Table(catalog = "FunLearnDB", schema = "")
+@XmlRootElement
 public class Story implements Serializable {
-	/**
-	 * 
-	 */
-	private static final long serialVersionUID = -7709726697403991641L;
-	private int storyID;
-	private String storyName;
-	private int ageGroup;
-	private String language;
-	private Map<Integer,String> paragraph;
-	private Map<Integer,String> picturePath;
-	private Map<Integer,String> musicAudioPath;
-	private Map<Integer,String> paragraphAudioPath;
-	/**
-	 * @param storyID
-	 * @param storyName
-	 * @param ageGroup
-	 * @param language
-	 * @param paragraph
-	 * @param picturePath
-	 * @param musicAudioPath
-	 * @param paragraphAudioPath
-	 */
-	public Story(int storyID, String storyName, int ageGroup, String language,
-			Map<Integer, String> paragraph, Map<Integer, String> picturePath,
-			Map<Integer, String> musicAudioPath,
-			Map<Integer, String> paragraphAudioPath) {
-		this.storyID = storyID;
-		this.storyName = storyName;
-		this.ageGroup = ageGroup;
-		this.language = language;
-		this.paragraph = paragraph;
-		this.picturePath = picturePath;
-		this.musicAudioPath = musicAudioPath;
-		this.paragraphAudioPath = paragraphAudioPath;
-	}
-	public Story() {
-		
-	}
-	public int getStoryID() {
-		return storyID;
-	}
-	public void setStoryID(int storyID) {
-		this.storyID = storyID;
-	}
-	public String getStoryName() {
-		return storyName;
-	}
-	public void setStoryName(String storyName) {
-		this.storyName = storyName;
-	}
-	public int getAgeGroup() {
-		return ageGroup;
-	}
-	public void setAgeGroup(int ageGroup) {
-		this.ageGroup = ageGroup;
-	}
-	public String getLanguage() {
-		return language;
-	}
-	public void setLanguage(String language) {
-		this.language = language;
-	}
-	public Map<Integer, String> getParagraph() {
-		return paragraph;
-	}
-	public void setParagraph(Map<Integer, String> paragraph) {
-		this.paragraph = paragraph;
-	}
-	public Map<Integer, String> getPicturePath() {
-		return picturePath;
-	}
-	public void setPicturePath(Map<Integer, String> picturePath) {
-		this.picturePath = picturePath;
-	}
-	public Map<Integer, String> getMusicAudioPath() {
-		return musicAudioPath;
-	}
-	public void setMusicAudioPath(Map<Integer, String> musicAudioPath) {
-		this.musicAudioPath = musicAudioPath;
-	}
-	public Map<Integer, String> getParagraphAudioPath() {
-		return paragraphAudioPath;
-	}
-	public void setParagraphAudioPath(Map<Integer, String> paragraphAudioPath) {
-		this.paragraphAudioPath = paragraphAudioPath;
-	}
+    private static final long serialVersionUID = 1L;
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Basic(optional = false)
+    @Column(nullable = false)
+    private Integer storyID;
+    @Basic(optional = false)
+    @Column(nullable = false, length = 45)
+    private String storyName;
+    @Basic(optional = false)
+    @Column(nullable = false)
+    private int ageGroup;
+    @Column(length = 45)
+    private String language;
+    @Basic(optional = false)
+    @Column(nullable = false, length = 45)
+    private String storyVideoPath;
+
+    public Story() {
+    }
+
+    public Story(Integer storyID) {
+        this.storyID = storyID;
+    }
+
+    public Story(Integer storyID, String storyName, int ageGroup, String storyVideoPath) {
+        this.storyID = storyID;
+        this.storyName = storyName;
+        this.ageGroup = ageGroup;
+        this.storyVideoPath = storyVideoPath;
+    }
+
+    public Integer getStoryID() {
+        return storyID;
+    }
+
+    public void setStoryID(Integer storyID) {
+        this.storyID = storyID;
+    }
+
+    public String getStoryName() {
+        return storyName;
+    }
+
+    public void setStoryName(String storyName) {
+        this.storyName = storyName;
+    }
+
+    public int getAgeGroup() {
+        return ageGroup;
+    }
+
+    public void setAgeGroup(int ageGroup) {
+        this.ageGroup = ageGroup;
+    }
+
+    public String getLanguage() {
+        return language;
+    }
+
+    public void setLanguage(String language) {
+        this.language = language;
+    }
+
+    public String getStoryVideoPath() {
+        return storyVideoPath;
+    }
+
+    public void setStoryVideoPath(String storyVideoPath) {
+        this.storyVideoPath = storyVideoPath;
+    }
+
+    @Override
+    public int hashCode() {
+        int hash = 0;
+        hash += (storyID != null ? storyID.hashCode() : 0);
+        return hash;
+    }
+
+    @Override
+    public boolean equals(Object object) {
+        // TODO: Warning - this method won't work in the case the id fields are not set
+        if (!(object instanceof Story)) {
+            return false;
+        }
+        Story other = (Story) object;
+        if ((this.storyID == null && other.storyID != null) || (this.storyID != null && !this.storyID.equals(other.storyID))) {
+            return false;
+        }
+        return true;
+    }
+
+    @Override
+    public String toString() {
+        return "model.Story[ storyID=" + storyID + " ]";
+    }
+    
 }
