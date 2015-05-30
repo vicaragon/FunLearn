287a96aa9dc8e27eaea21bf8facd2cd9e4b47b07
Sonam Rudraraju <sonam.rks@gmail.com>
2015-05-30 13:55:57 -0700

    GUI changes

--- src/view/SetupUI.java
+++ src/view/SetupUI.java
@@ -1,29 +1,15 @@
 package view;
 
-import javax.swing.JFrame;
-import controller.*;
-import javax.swing.JPanel;
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
 
+/**
+ *
+ * @author Sonam
+ */
 public class SetupUI {
-	private JFrame window;
-	private JPanel cards;
-	private StudentsListUI studentListUI;
-	private GameTypeUI gameTypeUI;
-	private LevelTypeUI levelTypeUI;
-	private StartUI startUI;
-	private GameUI gameUI;
-	private StoryUI storyUI;
-	private SongUI songUI;
-	public SetupUI() {
-		cards.add(studentListUI);
-		cards.add(gameTypeUI);
-		cards.add(levelTypeUI);
-		cards.add(startUI);
-	}
-	
-	public static void main (String[] args) {
-		
-	}
-		
-
+    
 }
