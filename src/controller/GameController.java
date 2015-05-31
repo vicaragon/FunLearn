006397a0d3be0257ab10e2cbd26095d14b342fa1
Sonam Rudraraju <sonam.rks@gmail.com>
2015-05-30 17:12:35 -0700

    Audiostream to file

--- src/controller/GameController.java
+++ src/controller/GameController.java
@@ -1,11 +1,11 @@
 package controller;
+import java.io.File;
 import java.util.List;
 import java.util.Map;
 import javax.swing.ImageIcon;
 import javax.swing.event.TableModelEvent;
 import javax.swing.event.TableModelListener;
 
-import sun.audio.AudioStream;
 import view.*;
 import model.*;
 
@@ -53,25 +53,25 @@ public class GameController implements TableModelListener {
 	public ImageIcon retrievePicture() {
 		return null;
 	}
-	public AudioStream retrieveMusic() {
+	public File retrieveMusic() {
 		return null;
 	}
 	public String retrieveQuestion() {
 		return null;
 	}
-	public AudioStream retrieveQuestionAudio() {
+	public File retrieveQuestionAudio() {
 		return null;
 	}
 	public String retrieverightAnswer() {
 		return null;
 	}
-	public AudioStream  retrieverightAnswerAudio() {
+	public File  retrieverightAnswerAudio() {
 		return null;
 	}
 	public List<String> retrieveAnswers() {
 		return null;
 	}
-	public List<AudioStream> retrieveAnswersAudio() {
+	public List<File> retrieveAnswersAudio() {
 		return null;
 	}
 
