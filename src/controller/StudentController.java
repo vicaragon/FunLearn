6cc7f63c14afda5e72a9e011a43a212ea2b3a778
Sonam Rudraraju <sonam.rks@gmail.com>
2015-06-01 20:11:46 -0700

    configuring tables in Student UI and controller

--- src/controller/StudentController.java
+++ src/controller/StudentController.java
@@ -3,8 +3,11 @@ package controller;
 import javax.swing.event.TableModelEvent;
 import javax.swing.event.TableModelListener;
 import view.StudentsListUI;
+import javax.swing.table.TableModel;
+import javax.swing.event.*;
+import javax.swing.ListSelectionModel;
 
-public class StudentController implements TableModelListener {
+public class StudentController implements ListSelectionListener, TableModelListener {
 	private StudentTableModel studentTablemodel;
         private StudentsListUI studentsListUI;
 	public StudentController(StudentsListUI studentsListUI) {
@@ -12,6 +15,40 @@ public class StudentController implements TableModelListener {
             studentTablemodel = new StudentTableModel();
             studentTablemodel.addTableModelListener(this);
         }
+        
+        public TableModel getTableModel() {
+		return studentTablemodel;
+	}
+        
+	public void valueChanged(ListSelectionEvent e) {
+            ListSelectionModel selectModel = (ListSelectionModel) e.getSource();
+            int firstIndex = selectModel.getMinSelectionIndex();
+		
+            studentsListUI.setNameTextField( (String) studentTablemodel.getValueAt(firstIndex, 0));
+            studentsListUI.setAgeTextField( (String) studentTablemodel.getValueAt(firstIndex, 1));
+	}   
+        
+        public void tableChanged(TableModelEvent e) {
+	   try {
+	    	// get the index of the inserted row
+	        //tableModel.getRowSet().moveToCurrentRow();
+	    	int firstIndex =  e.getFirstRow();
+	    	
+	    	// create a new table model with the new data
+	        studentTablemodel = new StudentTablemodel(studentTablemodel.getList(), studentTablemodel.getEntityManager());
+	        studentTablemodel.addTableModelListener(this);
+	        // update the JTable with the data
+	    	studentsListUI.updateTable1();
+	    
+	        studentsListUI.setNameTextField( (String) studentTablemodel.getValueAt(firstIndex, 0));
+                studentsListUI.setAgeTextField( (String) studentTablemodel.getValueAt(firstIndex, 1));
+
+	} catch(Exception exp) {
+		exp.getMessage();
+		exp.printStackTrace();
+	}
+	}
+        
 	public void findStudentNames(String name){
 		
 	}
@@ -36,9 +73,5 @@ public class StudentController implements TableModelListener {
 		
 	}
 
-    @Override
-    public void tableChanged(TableModelEvent tme) {
-        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
-    }
 		
 }
