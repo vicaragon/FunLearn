cb830bb56c92c4373077ae59dca46ba505720d3f
robertoramsis <robertoramsis@yahoo.com>
2015-06-11 22:01:00 -0700

    GameUI enhancements

--- src/view/GameUI.java
+++ src/view/GameUI.java
@@ -279,9 +279,10 @@ public class GameUI extends javax.swing.JFrame {
         jButton1 = new javax.swing.JButton();
         jButton2 = new javax.swing.JButton();
         jButton3 = new javax.swing.JButton();
-        jLabel1 = new javax.swing.JLabel();
         jLabel4 = new javax.swing.JLabel();
         jLabel5 = new javax.swing.JLabel();
+        jLabel7 = new javax.swing.JLabel();
+        jLabel8 = new javax.swing.JLabel();
         jLabel6 = new javax.swing.JLabel();
 
         setDefaultCloseOperation(javax.swing.WindowConstants.DO_NOTHING_ON_CLOSE);
@@ -310,45 +311,53 @@ public class GameUI extends javax.swing.JFrame {
         jPanel2.setBounds(300, 50, 380, 270);
 
         jLabel2.setFont(new java.awt.Font("Tahoma", 0, 36)); // NOI18N
+        jLabel2.setHorizontalAlignment(javax.swing.SwingConstants.CENTER);
         jLabel2.setText("question");
         jPanel1.add(jLabel2);
-        jLabel2.setBounds(130, 360, 600, 70);
+        jLabel2.setBounds(20, 330, 960, 130);
 
         jButton1.setBackground(new java.awt.Color(204, 0, 51));
         jButton1.setFont(new java.awt.Font("Tahoma", 1, 36)); // NOI18N
         jButton1.setForeground(new java.awt.Color(204, 255, 255));
         jButton1.setText("option1");
         jPanel1.add(jButton1);
-        jButton1.setBounds(140, 470, 210, 130);
+        jButton1.setBounds(110, 470, 210, 130);
 
         jButton2.setBackground(new java.awt.Color(204, 0, 51));
         jButton2.setFont(new java.awt.Font("Tahoma", 1, 36)); // NOI18N
         jButton2.setForeground(new java.awt.Color(204, 255, 255));
         jButton2.setText("option2");
         jPanel1.add(jButton2);
-        jButton2.setBounds(410, 470, 210, 130);
+        jButton2.setBounds(380, 470, 210, 130);
 
         jButton3.setBackground(new java.awt.Color(204, 0, 51));
         jButton3.setFont(new java.awt.Font("Tahoma", 1, 36)); // NOI18N
         jButton3.setForeground(new java.awt.Color(204, 255, 255));
         jButton3.setText("option3");
         jPanel1.add(jButton3);
-        jButton3.setBounds(690, 470, 200, 130);
-
-        jLabel1.setText("Question:");
-        jPanel1.add(jLabel1);
-        jLabel1.setBounds(30, 280, 100, 30);
+        jButton3.setBounds(660, 470, 200, 130);
 
         jLabel4.setFont(new java.awt.Font("Tahoma", 0, 24)); // NOI18N
-        jLabel4.setText("jLabel4");
+        jLabel4.setText("name");
         jPanel1.add(jLabel4);
-        jLabel4.setBounds(770, 50, 110, 50);
+        jLabel4.setBounds(770, 100, 110, 50);
 
-        jLabel5.setFont(new java.awt.Font("Times New Roman", 0, 24)); // NOI18N
+        jLabel5.setFont(new java.awt.Font("Tahoma", 1, 24)); // NOI18N
         jLabel5.setForeground(new java.awt.Color(255, 0, 0));
-        jLabel5.setText("jLabel5");
+        jLabel5.setText("value");
         jPanel1.add(jLabel5);
-        jLabel5.setBounds(890, 60, 80, 30);
+        jLabel5.setBounds(880, 110, 80, 30);
+
+        jLabel7.setFont(new java.awt.Font("Tahoma", 0, 24)); // NOI18N
+        jLabel7.setText("Player");
+        jPanel1.add(jLabel7);
+        jLabel7.setBounds(770, 60, 80, 30);
+
+        jLabel8.setFont(new java.awt.Font("Tahoma", 1, 24)); // NOI18N
+        jLabel8.setForeground(new java.awt.Color(255, 0, 0));
+        jLabel8.setText("Score");
+        jPanel1.add(jLabel8);
+        jLabel8.setBounds(880, 60, 70, 30);
 
         jLabel6.setIcon(new javax.swing.ImageIcon(getClass().getResource("/META-INF/gbg.jpg"))); // NOI18N
         jPanel1.add(jLabel6);
@@ -391,12 +400,13 @@ public class GameUI extends javax.swing.JFrame {
     private javax.swing.JButton jButton1;
     private javax.swing.JButton jButton2;
     private javax.swing.JButton jButton3;
-    private javax.swing.JLabel jLabel1;
     private javax.swing.JLabel jLabel2;
     private javax.swing.JLabel jLabel3;
     private javax.swing.JLabel jLabel4;
     private javax.swing.JLabel jLabel5;
     private javax.swing.JLabel jLabel6;
+    private javax.swing.JLabel jLabel7;
+    private javax.swing.JLabel jLabel8;
     private javax.swing.JPanel jPanel1;
     private javax.swing.JPanel jPanel2;
     // End of variables declaration//GEN-END:variables
